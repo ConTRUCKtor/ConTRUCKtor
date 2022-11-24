5bba120170662467b1d867603d0f41dc70ff00ea
Elvin Bajric <190026@studierende.htl-donaustadt.at>
2022-11-24 12:33:27 +0000

    solidjs ready

---
+++ front_end/vite.config.js
@@ -0,0 +1,13 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [
+    solid({
+      ssr: false,
+    }),
+  ],
+  server: {
+    port: 80,
+    host: true,
+  },
+});
