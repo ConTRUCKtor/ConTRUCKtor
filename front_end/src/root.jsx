627ccb7549a121b09694f9e288a61cd2f59d8528
Elvin Bajric <190026@studierende.htl-donaustadt.at>
2022-11-29 00:27:35 +0100

    added solidjs with bootstrap

--- front_end/src/root.jsx
+++ front_end/src/root.jsx
@@ -20,7 +20,9 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossOrigin="anonymous"/>
+        <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+              crossOrigin="anonymous"/>
       </Head>
       <Body>
         <Suspense>
