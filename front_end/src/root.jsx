5e21392fd5f6f67328ce61c47cf72187bbe9ead4
Elvin Bajric <190026@studierende.htl-donaustadt.at>
2022-11-29 11:39:42 +0100

    added bootstrap properly

--- front_end/src/root.jsx
+++ front_end/src/root.jsx
@@ -17,7 +17,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>ConTRUCKtor</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -27,8 +27,6 @@ export default function Root() {
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
             <Routes>
               <FileRoutes />
             </Routes>
