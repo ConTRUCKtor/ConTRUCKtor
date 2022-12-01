e5de351542a3f6fbe631128301466b3a2f7d4e0e
Elvin Bajric <elvinbajricc@gmail.com>
2022-12-01 13:15:46 +0100

    added profile

---
+++ front_end/pages/profile.js
@@ -0,0 +1,20 @@
+import { useUser } from "@auth0/nextjs-auth0"
+
+export default function Profile(){
+    const {user, isLoading} = useUser();
+    
+    if(isLoading){
+        return <div>Loading...</div>;
+    }
+
+    let toShow;
+    if(user != null){
+        toShow = <img src={user.picture} className="img-thumbnail" alt="THIS IS A PICTURE OF YOUR STUPID FACE"/>;
+    }
+
+    return(
+        <>
+        {toShow}
+        </>
+    )
+}
