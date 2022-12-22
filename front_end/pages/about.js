1703a932d4e45c6b5d6f0e6feb286bd84f57b7cb
Elvin Bajric <190026@studierende.htl-donaustadt.at>
2022-12-22 14:14:44 +0100

    fixed about

--- front_end/pages/about.js
+++ front_end/pages/about.js
@@ -1,39 +1,16 @@
 import {useUser} from "@auth0/nextjs-auth0";
-
-function printName(useTemplate){
-    if(useTemplate == true)
-    {
-        return "Guest"
-    } else {
-        const {user, loading} = useUser();
-        return user.name
-    }
-}
-
 export default function About() {
 
-    const {user, loading} = useUser();
-    let useTemplate = false
+    const {user, isLoading} = useUser();
 
-    if(user === undefined)
-        useTemplate = true
+    if(isLoading)
+        return <p>Loading...</p>
 
-    var styles = StyleSheet.create({
-        headline: {
-            textAlign: 'center', // <-- the magic
-            fontWeight: 'bold',
-            fontSize: 18,
-            marginTop: 0,
-            width: 200,
-            backgroundColor: 'yellow',
-        }
-    })
+    let username = user ? user.name : "Guest";
 
     return (
         <>
-
-
-            <h3 > Hello printName(useTemplate)! </h3>
+            <h3 className="text-center"> Hello {username}! </h3>
 
             <div className="alert alert-primary" role="alert">
                 A simple primary alert—check it out!
@@ -43,7 +20,7 @@ export default function About() {
                 <img className="card-img-top" src=" " alt="Card image cap"/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text"> test: {printName(useTemplate)} </p>
+                    <p className="card-text"> test: {username} </p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
